493aa43a027a00d8f456e79440c7e5ebc464e251
thiremath <143859257+thiremath@users.noreply.github.com>
2023-10-09 00:55:09 -0400

    Time: 25 ms (7.79%), Space: 41.4 MB (98.73%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,17 +1,19 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        HashMap<Character,Integer> sHash = new HashMap<Character,Integer>() ;
-        HashMap<Character,Integer> tHash = new HashMap<Character,Integer>() ;
+        HashMap<Character,Integer> Hash = new HashMap<Character,Integer>() ;
         if(s.length() != t.length()){
             return false ;
         }
         for(int i=0;i<s.length();i++){
-            sHash.put(s.charAt(i), sHash.getOrDefault(s.charAt(i), 0) + 1);
-            tHash.put(t.charAt(i), tHash.getOrDefault(t.charAt(i), 0) + 1);
+            Hash.put(s.charAt(i), Hash.getOrDefault(s.charAt(i), 0) + 1);
+            Hash.put(t.charAt(i), Hash.getOrDefault(t.charAt(i), 0) - 1);
         }
-        if(sHash.equals(tHash)){
-            return true ;
+        for(int val: Hash.values()){
+            if(val != 0){
+                return false ;
+            }
         }
-        return false ;
+        
+        return true ;
     }
 }
