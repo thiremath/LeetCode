6fab0c4824e2f2b569323905ebccc6c20324013b
thiremath <143859257+thiremath@users.noreply.github.com>
2023-10-09 00:54:37 -0400

    Time: 24 ms (8.99%), Space: 44 MB (12.01%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,19 +1,17 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        HashMap<Character,Integer> Hash = new HashMap<Character,Integer>() ;
+        HashMap<Character,Integer> sHash = new HashMap<Character,Integer>() ;
+        HashMap<Character,Integer> tHash = new HashMap<Character,Integer>() ;
         if(s.length() != t.length()){
             return false ;
         }
         for(int i=0;i<s.length();i++){
-            Hash.put(s.charAt(i), Hash.getOrDefault(s.charAt(i), 0) + 1);
-            Hash.put(t.charAt(i), Hash.getOrDefault(t.charAt(i), 0) - 1);
+            sHash.put(s.charAt(i), sHash.getOrDefault(s.charAt(i), 0) + 1);
+            tHash.put(t.charAt(i), tHash.getOrDefault(t.charAt(i), 0) + 1);
         }
-        for(int val: Hash.values()){
-            if(val != 0){
-                return false ;
-            }
+        if(sHash.equals(tHash)){
+            return true ;
         }
-        
-        return true ;
+        return false ;
     }
 }
