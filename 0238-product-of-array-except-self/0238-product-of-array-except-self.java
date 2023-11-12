7f920909211cf6a11dc1847445145b7312ae0048
thiremath <143859257+thiremath@users.noreply.github.com>
2023-11-12 12:21:57 -0500

    Time: 2 ms (65.08%), Space: 53.5 MB (22.91%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -6,21 +6,26 @@ class Solution {
         int[] ans = new int[n] ;
         prefix[0] = nums[0] ;
         postfix[n-1] = nums[n-1] ;
-        for(int i=1;i<n;i++){
-            prefix[i] = prefix[i-1] * nums[i] ;
-        }
+        // for(int i=1;i<n;i++){
+        //     prefix[i] = prefix[i-1] * nums[i] ;
+        // }
         for(int j=n-2;j>=0;j--){
             postfix[j] = postfix[j+1] * nums[j] ;
-            System.out.println(postfix[j]+" "+nums[1]) ;
         }
-        System.out.println(nums[1]) ;
-        System.out.println(nums[1]+" "+postfix[1]) ;
+
+        for(int i=1;i<n;i++){
+            prefix[i] = prefix[i-1] * nums[i] ;
+            if(i<n-1){
+                ans[i] = prefix[i-1] * postfix[i+1] ;
+            }
+        }
+
         ans[0] = postfix[1] ;
         ans[n-1] = prefix[n-2] ;
 
-        for(int k=1;k<=n-2;k++){
-            ans[k] = prefix[k-1] * postfix[k+1] ;
-        }
+        // for(int k=1;k<=n-2;k++){
+        //     ans[k] = prefix[k-1] * postfix[k+1] ;
+        // }
         return ans ;
         
     }
