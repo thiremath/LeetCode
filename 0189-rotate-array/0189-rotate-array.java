b1f1df72443c4a663670a5999d636b5b59a7e9e4
thiremath <143859257+thiremath@users.noreply.github.com>
2024-09-06 18:45:54 -0400

    Time: 2 ms (28.99%), Space: 57 MB (89.8%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -1,8 +1,6 @@
 class Solution {
     public void rotate(int[] nums, int k) {
-        if(k >= nums.length){
-            k = k % nums.length;
-        }
+        k = k % nums.length;
         if(k > 0){
             swapper(nums,0,nums.length-1);
             swapper(nums,0,k-1);
