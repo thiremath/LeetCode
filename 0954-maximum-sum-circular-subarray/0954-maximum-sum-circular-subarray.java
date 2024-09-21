47226005dfc5c3ff14c72700c0619396d0765005
thiremath <143859257+thiremath@users.noreply.github.com>
2024-09-21 19:36:57 -0400

    Time: 8 ms (17.72%), Space: 49.4 MB (20.68%) - LeetHub

--- 0954-maximum-sum-circular-subarray/0954-maximum-sum-circular-subarray.java
+++ 0954-maximum-sum-circular-subarray/0954-maximum-sum-circular-subarray.java
@@ -1,39 +1,17 @@
 class Solution {
     public int maxSubarraySumCircular(int[] nums) {
-        int ans = nums[0];
-        int minans = nums[0];
-        int currsum = 0;
-        int currminsum = 0;
-        int total = 0;
+        int ans=nums[0],minans = nums[0];
+        int currsum=0,currminsum=0,total = 0;
 
         for(int curr=0;curr<nums.length;curr++){
-
-            if(nums[curr]+currsum < nums[curr]){
-                currsum = nums[curr];
-            }
-            else{
-                currsum+=nums[curr];
-            }
-
-            if(nums[curr]+currminsum > nums[curr]){
-                currminsum = nums[curr];
-            }
-
-            else{
-                currminsum+=nums[curr];
-            }
-
+            currsum = Math.max(nums[curr]+currsum,nums[curr]);
+            currminsum = Math.min(nums[curr]+currminsum,nums[curr]);
             ans = Math.max(ans,currsum);
             minans = Math.min(minans,currminsum);
-
             total+=nums[curr];
         }
 
-        if(minans != total){
-            return Math.max(ans,total-minans);
-        }
-
-        return ans;
+        return (minans != total)? Math.max(ans,total-minans) : ans;
 
     }
 }
