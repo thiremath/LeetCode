2bce5812b690ee372a2bd6cbb1f57e9fac1f9cae
thiremath <143859257+thiremath@users.noreply.github.com>
2024-09-23 09:55:48 -0400

    Time: 3 ms (26.15%), Space: 43.8 MB (11.86%) - LeetHub

--- 0067-add-binary/0067-add-binary.java
+++ 0067-add-binary/0067-add-binary.java
@@ -1,53 +1,44 @@
 class Solution {
     public String addBinary(String a, String b) {
         StringBuilder ans = new StringBuilder("");
-        int index=0;
-        int carry=0;
+        int index=0,carry=0,tempa=0,tempb=0;
 
         while(a.length()-index > 0 && b.length()-index > 0){
-            int tempa = Integer.valueOf(String.valueOf(a.charAt(a.length()-index-1)));
-            int tempb = Integer.valueOf(String.valueOf(b.charAt(b.length()-index-1)));
+            tempa = Integer.valueOf(String.valueOf(a.charAt(a.length()-index-1)));
+            tempb = Integer.valueOf(String.valueOf(b.charAt(b.length()-index-1)));
 
-            // System.out.println(tempa+" "+tempb);
+            ans.append(Integer.toString((tempa+tempb+carry) % 2)) ;
 
             if(tempa+tempb+carry < 2){
-                ans.append(Integer.toString(tempa+tempb+carry)) ;
                 carry=0;
             }
-
-            else if(tempa+tempb+carry == 2){
-                ans.append("0");
-                carry = 1;
-            }
-
             else{
-                ans.append("1");
-                carry = 1;
+                carry=1;
             }
 
             index++;
         }
 
         while(a.length()-index > 0 || b.length()-index > 0){
-            int temp=0;
+            tempa = 0;
 
             if(a.length()-index > 0){
-                temp = Integer.valueOf(String.valueOf(a.charAt(a.length()-index-1)));
+                tempa = Integer.valueOf(String.valueOf(a.charAt(a.length()-index-1)));
             }
 
             else{
-                temp = Integer.valueOf(String.valueOf(b.charAt(b.length()-index-1)));
+                tempa = Integer.valueOf(String.valueOf(b.charAt(b.length()-index-1)));
             }
 
-            if(temp+carry < 2){
-                ans.append(Integer.toString(temp+carry)) ;
+            ans.append(Integer.toString((tempa+carry)%2)) ;
+            
+            if(tempa+carry < 2){
                 carry=0;
             }
-
-            else if(temp+carry == 2){
-                ans.append("0");
-                carry = 1;
+            else{
+                carry=1;
             }
+
 
             index++;
 
