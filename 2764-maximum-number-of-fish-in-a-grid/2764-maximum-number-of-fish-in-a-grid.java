7ec1642c03326b46bee1c2e1779154495d481033
thiremath <143859257+thiremath@users.noreply.github.com>
2025-01-28 13:56:42 -0500

    Time: 3 ms (100%), Space: 45.1 MB (43.7%) - LeetHub

--- 2764-maximum-number-of-fish-in-a-grid/2764-maximum-number-of-fish-in-a-grid.java
+++ 2764-maximum-number-of-fish-in-a-grid/2764-maximum-number-of-fish-in-a-grid.java
@@ -1,14 +1,13 @@
 class Solution {
     public int findMaxFish(int[][] grid) {
-        boolean[][] explored = new boolean[grid.length][grid[0].length];
         int res=0;
 
         for(int i=0;i<grid.length;i++){
 
             for(int j=0;j<grid[0].length;j++){
 
-                if(grid[i][j] > 0 && !explored[i][j]){
-                    res = Math.max(res, dfs(i,j,explored, grid));
+                if(grid[i][j] > 0){
+                    res = Math.max(res, dfs(i,j, grid));
                 }
             }
         }
@@ -16,10 +15,11 @@ class Solution {
         return res;
     }
 
-    public int dfs(int i, int j, boolean[][] explored, int[][] grid){
-        if(i < grid.length && i>=0 && j < grid[0].length && j>=0 && !explored[i][j] && grid[i][j] > 0){
-            explored[i][j] = true;
-            return grid[i][j] + dfs(i-1,j, explored, grid) + dfs(i,j-1, explored, grid) + dfs(i+1,j, explored, grid) + dfs(i,j+1, explored, grid) ;
+    public int dfs(int i, int j, int[][] grid){
+        if(i < grid.length && i>=0 && j < grid[0].length && j>=0 && grid[i][j] > 0){
+            int temp = grid[i][j];
+            grid[i][j] = 0;
+            return temp + dfs(i-1,j, grid) + dfs(i,j-1, grid) + dfs(i+1,j, grid) + dfs(i,j+1, grid) ;
         }
 
         return 0;
