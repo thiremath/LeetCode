da0422e6535d49662420b19864c77e5b6bd8891f
thiremath <143859257+thiremath@users.noreply.github.com>
2025-02-07 12:30:17 -0500

    Time: 48 ms (20.96%), Space: 102.1 MB (5.24%) - LeetHub

--- 3434-find-the-number-of-distinct-colors-among-the-balls/3434-find-the-number-of-distinct-colors-among-the-balls.java
+++ 3434-find-the-number-of-distinct-colors-among-the-balls/3434-find-the-number-of-distinct-colors-among-the-balls.java
@@ -3,31 +3,29 @@ class Solution {
 
         HashMap<Integer,Integer> ballmap = new HashMap<>();
         HashMap<Integer,List<Integer>> colmap = new HashMap<>();
-        int resindex = 0;
         int[] res = new int[queries.length];
 
-        for(int[] curr: queries){
+        for(int i=0;i<queries.length;i++){
 
-            if(ballmap.containsKey(curr[0])){
+            if(ballmap.containsKey(queries[i][0])){
 
-                int col = ballmap.get(curr[0]);
-                colmap.get(col).remove(Integer.valueOf(curr[0]));
+                int col = ballmap.get(queries[i][0]);
+                colmap.get(col).remove(Integer.valueOf(queries[i][0]));
                 if(colmap.get(col).size() == 0){
 
                     colmap.remove(col);
                 }
             }
 
-            ballmap.put(curr[0],curr[1]);
+            ballmap.put(queries[i][0],queries[i][1]);
 
-            if(!colmap.containsKey(curr[1])){
-                colmap.put(curr[1],new ArrayList<Integer>());
+            if(!colmap.containsKey(queries[i][1])){
+                colmap.put(queries[i][1],new ArrayList<Integer>());
             }
 
-            List<Integer> temp = colmap.get(curr[1]);
-            temp.add(curr[0]);
+            colmap.get(queries[i][1]).add(queries[i][0]);
 
-            res[resindex++] = colmap.size();
+            res[i] = colmap.size();
         }
 
         return res;
