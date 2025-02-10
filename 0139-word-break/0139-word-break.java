671e61b7ae21e1e9ea506e93fd610bb298aec68b
thiremath <143859257+thiremath@users.noreply.github.com>
2025-02-10 13:16:16 -0500

    Time: 13 ms (6.52%), Space: 45.3 MB (6.26%) - LeetHub

--- 0139-word-break/0139-word-break.java
+++ 0139-word-break/0139-word-break.java
@@ -19,7 +19,7 @@ class Solution {
             return 0;
         }
 
-        if(end == s.length()-1 && set.contains(s.substring(start,end+1))){
+        if(set.contains(s.substring(start,s.length()))){
             return 1;
         }
 
