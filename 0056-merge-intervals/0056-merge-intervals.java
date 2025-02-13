bfda2341605ed306ed47dc992b227216753664ba
thiremath <143859257+thiremath@users.noreply.github.com>
2025-02-13 00:27:27 -0500

    Time: 4439 ms (5.27%), Space: 235.2 MB (5.2%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -0,0 +1,94 @@
+class Solution {
+    public int[][] merge(int[][] intervals) {
+
+        boolean[][] edges = new boolean[intervals.length][intervals.length];
+        boolean[] isconn = new boolean[intervals.length];
+        ArrayList<int[]> ls = new ArrayList<int[]>();
+
+        for(int i=0;i<intervals.length-1;i++){
+
+            int a1 = intervals[i][0];
+            int a2 = intervals[i][1];
+
+            for(int j=i+1;j<intervals.length;j++){
+
+                int a3 = intervals[j][0];
+                int a4 = intervals[j][1];
+
+                if(a1 > a3){
+                    if(a4 < a1){
+                        continue;
+                    }
+                }
+                else if(a1 < a3){
+                    if(a2 < a3){
+                        continue;
+                    }
+                }
+                isconn[i] = true;
+                isconn[j] = true;
+                edges[i][j] = true;
+                edges[j][i] = true;
+            }
+        }
+
+        boolean[] conn = Arrays.copyOf(isconn,isconn.length);
+        // System.out.println(conn[1]);
+        Queue<Integer> q = new LinkedList<Integer>();
+
+        for(int i=0;i<edges.length;i++){
+
+            int min = intervals[i][0];
+            int max = intervals[i][1];
+            if(conn[i]){
+
+                for(int j=i+1;j<edges.length;j++){
+                    if(conn[j]){
+                        if(edges[i][j]){
+                            isconn[i] = false;
+                            min = Math.min(min,intervals[j][0]);
+                            max = Math.max(max,intervals[j][1]);
+                            edges[i][j] = false;
+                            edges[j][i] = false;
+                            q.add(j);
+                            while(q.size() > 0){
+                                int k = q.remove();
+                                // System.out.println(conn[1]);
+                                if(conn[k]){
+                                    for(int x=0;x<edges.length;x++){
+
+                                        if(edges[k][x]){
+                                            System.out.println("hii");
+                                            edges[k][x] = false;
+                                            edges[x][k] = false;
+                                            q.add(x);
+                                            min = Math.min(min,intervals[x][0]);
+                                            max = Math.max(max,intervals[x][1]);
+                                        }
+                                    }
+                                }
+                                conn[k] = false;
+                            }
+                        }
+                    }
+                    // conn[j] = false;
+                }
+            }
+            conn[i] = false;
+
+            if(!isconn[i]){
+                int[] temp = new int[]{min,max};
+                // System.out.println("["+min+","+max+"]");
+                ls.add(temp);
+            }
+        }
+        
+        int[][] ans = new int[ls.size()][2];
+        for(int i=0;i<ls.size();i++){
+
+            ans[i] = ls.get(i);
+        }
+
+        return ans;
+    }
+}
