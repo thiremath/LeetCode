7c39fe1288a6771b006f60964ba4c56fea9c92d0
thiremath <143859257+thiremath@users.noreply.github.com>
2025-02-17 13:03:31 -0500

    Time: 197 ms (5.03%), Space: 47.9 MB (5.2%) - LeetHub

---
+++ 1160-letter-tile-possibilities/1160-letter-tile-possibilities.java
@@ -0,0 +1,83 @@
+class Solution {
+
+    HashSet<String> set = new HashSet<String>();
+
+    public int numTilePossibilities(String tiles) {
+
+        numTilePossibilitiesWorker(tiles,tiles.length()-1,"");
+
+        for(String s: set){
+            System.out.println(s);
+        }
+
+        return set.size();
+    }
+
+    public void numTilePossibilitiesWorker(String tiles, int index, String st){
+
+        if(index == -1){
+            return ;
+        }
+
+        if(st.length() == 0){
+            set.add(String.valueOf(tiles.charAt(index)));
+            numTilePossibilitiesWorker(tiles,index-1,String.valueOf(tiles.charAt(index)));
+            numTilePossibilitiesWorker(tiles,index-1,"");
+        }
+
+        numTilePossibilitiesWorker(tiles,index-1,st);
+
+        for(int i=0;i<st.length()+1;i++){
+            String before = st.substring(0,i);
+            String after = st.substring(i,st.length());
+            char temp = tiles.charAt(index);
+
+            String newstr = before+temp+after;
+            
+            // System.out.println(newstr);
+            set.add(newstr);
+
+            numTilePossibilitiesWorker(tiles,index-1,newstr);
+        }
+        
+
+    }
+}
+
+
+// AAB
+
+// ""
+
+// B
+
+// AB
+// BA
+
+// AAB
+// ABA
+
+// BAA
+// BAB
+
+
+// ABC
+
+// ""
+
+// C
+// ""
+
+// BC
+// CB
+// C
+// B
+
+// ABC
+// BAC
+// BCA
+// ACB
+// CAB
+// CBA
+// AC
+// CA
