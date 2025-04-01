fb0d45e86b6e51200d769dbf7ab637eaf955303f
thiremath <143859257+thiremath@users.noreply.github.com>
2025-03-31 21:05:35 -0400

    Time: 6 ms (67.5%), Space: 108 MB (85%) - LeetHub

--- 2262-solving-questions-with-brainpower/2262-solving-questions-with-brainpower.java
+++ 2262-solving-questions-with-brainpower/2262-solving-questions-with-brainpower.java
@@ -1,8 +1,8 @@
 class Solution {
     public long mostPoints(int[][] questions) {
         int n = questions.length;
-        long[] dp = new long[n];
-        long res=1;
+        long[] dp = new long[n+1];
+        // long res=1;
 
         for(int i=n-1;i>=0;i--){
             int points = questions[i][0];
@@ -11,10 +11,9 @@ class Solution {
             if(i+power+1 <= n-1){
                 dp[i] += dp[i+power+1];
             }
-            dp[i] = Math.max(dp[i],res);
-            res = dp[i];
+            dp[i] = Math.max(dp[i],dp[i+1]);
         }
 
-        return res;
+        return dp[0];
     }
 }
