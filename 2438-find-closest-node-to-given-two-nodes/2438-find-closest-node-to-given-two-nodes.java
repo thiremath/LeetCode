8b6d066fc2eec46f28945a78a3939d673450d9e0
thiremath <143859257+thiremath@users.noreply.github.com>
2025-05-30 11:00:32 -0400

    Time: 43 ms (26.15%), Space: 61.5 MB (64.31%) - LeetHub

---
+++ 2438-find-closest-node-to-given-two-nodes/2438-find-closest-node-to-given-two-nodes.java
@@ -0,0 +1,45 @@
+class Solution {
+    public int closestMeetingNode(int[] edges, int node1, int node2) {
+        int n = edges.length;
+        int[] distances_n1 = new int[n];
+        int[] distances_n2 = new int[n];
+        Arrays.fill(distances_n1,-1);
+        Arrays.fill(distances_n2,-1);
+        int ans = Integer.MAX_VALUE;
+        int node=-1;
+
+        bfs(node1,edges,distances_n1);
+        bfs(node2,edges,distances_n2);
+
+        for(int i=0;i<n;i++){
+            if(distances_n1[i] != -1 && distances_n2[i] != -1){
+                if(ans > Math.max(distances_n1[i],distances_n2[i])){
+                    ans = Math.max(distances_n1[i],distances_n2[i]);
+                    node = i;
+                }
+            }
+        }
+
+        return node;
+    }
+
+    public void bfs(int node, int[] edges, int[] distances){
+
+        Queue<Integer> queue = new LinkedList<>();
+        HashSet<Integer> set = new HashSet<>();
+        int ans = -1;
+
+        queue.add(node);
+        set.add(node);
+
+        while(!queue.isEmpty()){
+            int curr = queue.poll();
+            distances[curr] = ++ans;
+            if(!set.contains(edges[curr]) && edges[curr] != -1){
+                set.add(edges[curr]);
+                queue.add(edges[curr]);
+            }
+        }
+
+    }
+}
