8f34953d5f1909b9a889ab1536b690de7cf95d00
thiremath <143859257+thiremath@users.noreply.github.com>
2025-06-07 13:18:37 -0400

    Time: 100 ms (86.67%), Space: 48.3 MB (60.61%) - LeetHub

---
+++ 3445-lexicographically-minimum-string-after-removing-stars/3445-lexicographically-minimum-string-after-removing-stars.java
@@ -0,0 +1,36 @@
+class Solution {
+    public String clearStars(String s) {
+        Stack<Integer>[] st = new Stack[26];
+        for(int i=0;i<26;i++){
+            st[i] = new Stack<>();
+        }
+        StringBuilder ans = new StringBuilder("");
+
+        char[] arr = s.toCharArray();
+
+        for(int i=0;i<arr.length;i++){
+            char c = arr[i];
+
+            if(c != '*'){
+                st[c-'a'].push(i);
+            }
+
+            else{
+                for(int j=0;j<26;j++){
+                    if(st[j].size() > 0){
+                        arr[st[j].pop()] = '*';
+                        break;
+                    }
+                }
+            }
+        }
+
+        for(int i=0;i<arr.length;i++){
+            if(arr[i] != '*'){
+                ans.append(arr[i]);
+            }
+        }
+
+        return String.valueOf(ans);
+    }
+}
