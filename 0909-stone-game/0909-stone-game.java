c9e0dab090a706ca6cd1b7ebefd85b2636ac4ee0
thiremath <143859257+thiremath@users.noreply.github.com>
2025-06-07 10:42:18 -0400

    Time: 10 ms (18.34%), Space: 45.2 MB (13.87%) - LeetHub

---
+++ 0909-stone-game/0909-stone-game.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean stoneGame(int[] piles) {
+        int sum=0;
+        for(int pile: piles){
+            sum+=pile;
+        }
+        int n = piles.length;
+        int[][] dp = new int[n][n];
+        for(int i=0;i<n;i++){
+            Arrays.fill(dp[i],-1);
+        }
+        return (stoneGameWorker(piles,0,n-1,true,dp) > sum/2);
+    }
+
+    public int stoneGameWorker(int[] piles, int low, int high, boolean isAliceTurn,int[][] dp) {
+        if(low == high){
+            return piles[low];
+        }
+
+        if(dp[low][high] != -1){
+            return dp[low][high];
+        }
+
+        if(isAliceTurn){
+            return dp[low][high] = Math.max(piles[low]+stoneGameWorker(piles,low+1,high,false,dp), piles[high]+stoneGameWorker(piles,low,high-1,false,dp));
+        }
+
+        return dp[low][high] = Math.min(piles[low]+stoneGameWorker(piles,low+1,high,true,dp), piles[high]+stoneGameWorker(piles,low,high-1,true,dp));
+    }
+}
