a39b99333f612292e997cacd421a549880450f0e
thiremath <143859257+thiremath@users.noreply.github.com>
2025-06-08 15:36:59 -0400

    Time: 1 ms (100%), Space: 42.7 MB (17.18%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.java
+++ 0010-regular-expression-matching/0010-regular-expression-matching.java
@@ -1,5 +1,12 @@
+enum Result{
+    TRUE,
+    FALSE
+}
+
 class Solution {
+    Result[][] dp;
     public boolean isMatch(String s, String p) {
+        dp = new Result[s.length()][p.length()];
         return isMatchWorker(s.toCharArray(),p.toCharArray(),s.length()-1,p.length()-1);
     }
 
@@ -12,8 +19,12 @@ class Solution {
             return false;
         }
 
+        if(sIndex >= 0 && dp[sIndex][pIndex] != null){
+            return dp[sIndex][pIndex] == Result.TRUE;
+        }
+
         if(sIndex >= 0 && (s[sIndex] == p[pIndex] || p[pIndex] == '.')){
-            return isMatchWorker(s,p,sIndex-1,pIndex-1);
+            dp[sIndex][pIndex] = (isMatchWorker(s,p,sIndex-1,pIndex-1)) ? Result.TRUE : Result.FALSE;
         }
 
         if(p[pIndex] == '*'){
@@ -21,7 +32,15 @@ class Solution {
             if(sIndex >= 0 && (s[sIndex] == p[pIndex-1] || p[pIndex-1] == '.')){
                 a = isMatchWorker(s,p,sIndex-1,pIndex);
             }
-            return isMatchWorker(s,p,sIndex,pIndex-2)||a;
+            boolean b = isMatchWorker(s,p,sIndex,pIndex-2)||a;
+            if(sIndex >= 0){
+                dp[sIndex][pIndex] = (b) ? Result.TRUE : Result.FALSE;
+            }
+            return b;
+        }
+
+        if(sIndex >= 0){
+            return dp[sIndex][pIndex] == Result.TRUE;
         }
 
         return false;
