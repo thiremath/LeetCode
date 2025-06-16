25b95a4a5962185b491b6083e73cd831d8ec5370
thiremath <143859257+thiremath@users.noreply.github.com>
2025-06-15 21:19:00 -0400

    Time: 7 ms (97.58%), Space: 56.7 MB (74.31%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.java
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.java
@@ -3,11 +3,15 @@ class Solution {
 
     public List<List<String>> partition(String s) {
         ans = new ArrayList<>();
-        partitionWorker(s,0,new ArrayList<>());
+        int[][] dp = new int[s.length()][s.length()];
+        for(int i=0;i<s.length();i++){
+            Arrays.fill(dp[i],-1);
+        }
+        partitionWorker(s,0,new ArrayList<>(),dp);
         return ans;
     }
 
-    public void partitionWorker(String s,int index, List<String> curr) {
+    public void partitionWorker(String s,int index, List<String> curr, int[][] dp) {
         // System.out.println(curr);
         // System.out.println("ans "+ ans);
 
@@ -17,26 +21,33 @@ class Solution {
         }
         
         for(int i=index;i<s.length();i++){
-            String sub = s.substring(index,i+1);
-            if(isPalindrome(sub)){
-                curr.add(sub);
-                partitionWorker(s,i+1,curr);
+            if(isPalindrome(s,dp,index,i)){
+                curr.add(s.substring(index,i+1));
+                partitionWorker(s,i+1,curr,dp);
                 curr.remove(curr.size() - 1);
             }
         }
     }
 
-    public boolean isPalindrome(String s){
-        int low=0;
-        int high = s.length()-1;
+    public boolean isPalindrome(String s, int[][] dp, int l, int h){
 
-        while(low < high){
+        int low = l;
+        int high = h;
+
+        while(low <= high){
+            if(dp[low][high] != -1){
+                return dp[low][high] == 1;
+            }
             if(s.charAt(low) != s.charAt(high)){
+                dp[low][high] = 0;
                 return false;
             }
+            // dp[low][high] = 1;
             low++;
             high--;
         }
+
+        dp[l][h] = 1;
 
         return true;
     }
