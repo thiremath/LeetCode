1bd8d8ee14fd4cbab62222c5638fea86a6dd7da2
thiremath <143859257+thiremath@users.noreply.github.com>
2025-06-27 12:08:14 -0400

    Time: 198 ms (90.57%), Space: 45.8 MB (11.32%) - LeetHub

--- 2140-longest-subsequence-repeated-k-times/2140-longest-subsequence-repeated-k-times.java
+++ 2140-longest-subsequence-repeated-k-times/2140-longest-subsequence-repeated-k-times.java
@@ -1,65 +1,54 @@
 class Solution {
+
     public String longestSubsequenceRepeatedK(String s, int k) {
-        int[] letter_arr = new int[26];
-        char[] s_arr = s.toCharArray();
-        List<StringBuilder> candidates = new ArrayList<>();
-        Queue<StringBuilder> queue = new LinkedList<>();
-        StringBuilder ans = new StringBuilder("");
-        StringBuilder currans = new StringBuilder("");
-
-        for(char c: s_arr){
-            ++letter_arr[c-'a'];
+        int[] freq = new int[26];
+        for (char ch : s.toCharArray()) {
+            freq[ch - 'a']++;
         }
-
-        for(int i=25;i>=0;i--){
-            if(letter_arr[i] >= k){
-                StringBuilder lett = new StringBuilder(String.valueOf((char)('a'+i)));
-                // System.out.println(lett.toString());
-                queue.add(lett);
-                candidates.add(lett);
+        List<Character> candidate = new ArrayList<>();
+        for (int i = 25; i >= 0; i--) {
+            if (freq[i] >= k) {
+                candidate.add((char) ('a' + i));
             }
         }
 
-        while(!queue.isEmpty()){
-            currans = queue.poll();
-            if(ans.length() < currans.length()){
-                ans = currans;
+        Queue<String> q = new LinkedList<>();
+        for (char ch : candidate) {
+            q.add(String.valueOf(ch));
+        }
+        String ans = "";
+        while (!q.isEmpty()) {
+            String curr = q.poll();
+            if (curr.length() > ans.length()) {
+                ans = curr;
             }
-
-            for(StringBuilder candidate: candidates){
-                // System.out.println(currans.toString());
-                currans.append(candidate);
-                // System.out.println(currans.toString());
-                if(isSubsequenceRepeatedK(s_arr,k,currans)){
-                    // System.out.println("true");
-                    queue.add(new StringBuilder(currans));
+            // generate the next candidate string
+            for (char ch : candidate) {
+                String next = curr + ch;
+                if (isKRepeatedSubsequence(s, next, k)) {
+                    q.add(next);
                 }
-                currans.deleteCharAt(currans.length() - 1);
-                // System.out.println(currans.toString());
-                // System.out.println("");
             }
         }
 
-        return String.valueOf(ans);
+        return ans;
     }
 
-    public Boolean isSubsequenceRepeatedK(char[] s_arr,int k,StringBuilder currans){
-        int index = 0;
-        int sbLen = currans.length();
-        int curr_k=0;
-
-        for(char c: s_arr){
-            if(c == currans.charAt(index)){
-                if(++index == sbLen){
-                    if(++curr_k == k){
+    private boolean isKRepeatedSubsequence(String s, String t, int k) {
+        int pos = 0, matched = 0;
+        int m = t.length();
+        for (char ch : s.toCharArray()) {
+            if (ch == t.charAt(pos)) {
+                pos++;
+                if (pos == m) {
+                    pos = 0;
+                    matched++;
+                    if (matched == k) {
                         return true;
                     }
-                    index=0;
                 }
             }
         }
-
         return false;
-    }    
-
+    }
 }
