d703f692f9a0f1c85429f48c676e645a9001083f
thiremath <143859257+thiremath@users.noreply.github.com>
2025-06-29 17:15:00 -0400

    Time: 1343 ms (5.06%), Space: 48 MB (5.77%) - LeetHub

---
+++ 0135-candy/0135-candy.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int candy(int[] ratings) {
+        int n = ratings.length;
+        int[] candiesarr = new int[n];
+        int ans = 0;
+
+        for(int i=0;i<n;i++){
+            if(candiesarr[i] == 0){
+                candyworker(ratings,candiesarr,i);
+            }
+            ans+=candiesarr[i];
+        }
+
+        return ans;
+    }
+
+    public int candyworker(int[] ratings, int[] candiesarr, int index){
+        int leftans = -1, rightans = -1;
+
+        if(index-1 >= 0 && ratings[index] > ratings[index-1]){
+            leftans = candyworker(ratings,candiesarr,index-1);
+        }
+
+        if(index+1 < ratings.length && ratings[index] > ratings[index+1]){
+            rightans = candyworker(ratings,candiesarr,index+1);
+        }
+
+        if(leftans == -1 && rightans == -1){
+            return candiesarr[index] = 1;
+        }
+
+        return candiesarr[index] = 1+Math.max(leftans,rightans);
+    }   
+}
