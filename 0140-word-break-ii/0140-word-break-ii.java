d1d88e1c4d10a7badfb05a389a725e8b927b319e
thiremath <143859257+thiremath@users.noreply.github.com>
2025-07-15 17:46:10 -0400

    Time: 1 ms (93.48%), Space: 41.8 MB (67.95%) - LeetHub

--- 0140-word-break-ii/0140-word-break-ii.java
+++ 0140-word-break-ii/0140-word-break-ii.java
@@ -1,9 +1,9 @@
 class Solution {
-    List<String>[][] dp;
+    List<String>[] dp;
     Set<String> set;
 
     public List<String> wordBreak(String s, List<String> wordDict) {
-        dp = new ArrayList[s.length()][s.length()];
+        dp = new ArrayList[s.length()];
         set=new HashSet<>();
         set.addAll(wordDict);
         return wordBreakWorker(s,0,s.length()-1);
@@ -11,16 +11,16 @@ class Solution {
 
     public List<String> wordBreakWorker(String s, int low, int high) {
 
-        if(dp[low][high] != null){
-            return dp[low][high];
+        if(dp[low] != null){
+            return dp[low];
         }
 
-        dp[low][high] = new ArrayList<>();
+        dp[low] = new ArrayList<>();
 
         String currst = s.substring(low,high+1);
 
         if(set.contains(currst)){
-            dp[low][high].add(currst);
+            dp[low].add(currst);
         }
 
         for(int i=low+1;i<=high;i++){
@@ -29,11 +29,11 @@ class Solution {
                 List<String> next = wordBreakWorker(s,i,high);
                 for(String st: next){
                     StringBuilder currans = new StringBuilder(curr).append(" ").append(st);
-                    dp[low][high].add(String.valueOf(currans));   
+                    dp[low].add(String.valueOf(currans));   
                 }
             }
         }
 
-        return dp[low][high];
+        return dp[low];
     }
 }
