415a673314b5ac698ffd71697443311235444941
thiremath <143859257+thiremath@users.noreply.github.com>
2025-07-15 17:35:59 -0400

    Time: 1 ms (93.48%), Space: 41.6 MB (94.83%) - LeetHub

--- 0140-word-break-ii/0140-word-break-ii.java
+++ 0140-word-break-ii/0140-word-break-ii.java
@@ -11,8 +11,6 @@ class Solution {
 
     public List<String> wordBreakWorker(String s, int low, int high) {
 
-        // System.out.println("low,high = "+low+" "+high);
-
         if(dp[low][high] != null){
             return dp[low][high];
         }
@@ -20,7 +18,6 @@ class Solution {
         dp[low][high] = new ArrayList<>();
 
         String currst = s.substring(low,high+1);
-        // System.out.println("currst = "+currst);
 
         if(set.contains(currst)){
             dp[low][high].add(currst);
@@ -28,11 +25,8 @@ class Solution {
 
         for(int i=low+1;i<=high;i++){
             String curr = s.substring(low,i);
-            // System.out.println("curr = "+curr);
             if(set.contains(curr)){
-                System.out.println("currfound");
                 List<String> next = wordBreakWorker(s,i,high);
-                // System.out.println("next = "+next);
                 if(next.size()!=0){
                     for(String st: next){
                         StringBuilder currans = new StringBuilder(curr).append(" ").append(st);
