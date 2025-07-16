f4b83cf4144a0c682ba4f6a61620a0f1b76b580f
thiremath <143859257+thiremath@users.noreply.github.com>
2025-07-16 17:43:09 -0400

    Time: 5 ms (60.48%), Space: 62.3 MB (53.23%) - LeetHub

--- 3490-find-the-maximum-length-of-valid-subsequence-i/3490-find-the-maximum-length-of-valid-subsequence-i.java
+++ 3490-find-the-maximum-length-of-valid-subsequence-i/3490-find-the-maximum-length-of-valid-subsequence-i.java
@@ -1,7 +1,7 @@
 class Solution {
     public int maximumLength(int[] nums) {
-        int odd=0,even=0,alternate=0;
-        boolean prev = ((nums[0]+1)%2)==0;
+        int odd=0,alternate=0;
+        boolean prev = !(((nums[0])%2)==0);
 
         for(int num:nums){
             boolean isOdd = (num%2==0);
@@ -9,16 +9,13 @@ class Solution {
             if(isOdd){
                 odd++;
             }
-            else{
-                even++;
-            }
 
             if(prev != isOdd){
                 alternate++;
                 prev = isOdd;
             }
         }
-        return Math.max(alternate,Math.max(odd,even));
+        return Math.max(alternate,Math.max(odd,nums.length-odd));
     }
 }
 
