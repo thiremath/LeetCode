020ea6d3c6f763afa5482562fd07983ee626a3ce
thiremath <143859257+thiremath@users.noreply.github.com>
2025-07-16 23:10:12 -0400

    Time: 2292 ms (5.16%), Space: 55.3 MB (9.28%) - LeetHub

--- 3491-find-the-maximum-length-of-valid-subsequence-ii/3491-find-the-maximum-length-of-valid-subsequence-ii.java
+++ 3491-find-the-maximum-length-of-valid-subsequence-ii/3491-find-the-maximum-length-of-valid-subsequence-ii.java
@@ -1,47 +1,40 @@
 class Solution {
     Integer[][] dp;
+
     public int maximumLength(int[] nums, int k) {
-
         int n = nums.length;
+        int Max = 0;
 
-        for(int i=0;i<n;i++){
-            nums[i] = nums[i]%k;
+        for (int i = 0; i < n; i++) {
+            nums[i] %= k;
+            Max = Math.max(Max, nums[i]);
         }
 
-        int ans=0;
-        dp = new Integer[nums.length][k];
+        dp = new Integer[n][k + 1]; // using k as index for prev == -1
 
-        for(int i=0;i<n-1;i++){
-            if(ans >= n-i){
-                break;
-            }
-            ans = Math.max(ans,maximumLengthWorker(nums,i,-1));
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            if (ans >= n - i) break;
+            ans = Math.max(ans, maximumLengthWorker(nums, i, -1, k));
         }
 
         return ans;
     }
 
-    public int maximumLengthWorker(int[] nums, int index, int prev) {
+    public int maximumLengthWorker(int[] nums, int index, int prev, int k) {
+        int prevIndex = prev == -1 ? k : prev;
 
-        if(prev != -1 && dp[index][prev] != null){
-            return dp[index][prev];
-        }
+        if (dp[index][prevIndex] != null)
+            return dp[index][prevIndex];
 
-        int ans=1;
-
-        for(int i=index+1;i<nums.length;i++){
-            if(nums[i] == prev || prev == -1){
-                ans = Math.max(ans,1+maximumLengthWorker(nums,i,nums[index]));
+        int ans = 1;
+        for (int i = index + 1; i < nums.length; i++) {
+            if (prev == -1 || nums[i] == prev) {
+                ans = Math.max(ans, 1 + maximumLengthWorker(nums, i, nums[index], k));
             }
         }
 
-        if(prev != -1){
-            dp[index][prev] = ans;
-        }
-
+        dp[index][prevIndex] = ans;
         return ans;
     }
 }
-
-
-// 1,1,2,0,1,1
