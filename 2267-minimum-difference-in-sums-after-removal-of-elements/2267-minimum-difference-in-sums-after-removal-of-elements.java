109ad11a27fb2c31064dcf78c1fbf6c01be431fe
thiremath <143859257+thiremath@users.noreply.github.com>
2025-07-18 01:26:45 -0400

    Time: 322 ms (18.12%), Space: 78.4 MB (91.3%) - LeetHub

--- 2267-minimum-difference-in-sums-after-removal-of-elements/2267-minimum-difference-in-sums-after-removal-of-elements.java
+++ 2267-minimum-difference-in-sums-after-removal-of-elements/2267-minimum-difference-in-sums-after-removal-of-elements.java
@@ -2,10 +2,7 @@ class Solution {
     public long minimumDifference(int[] nums) {
         int n = nums.length/3;
         long ans = Long.MAX_VALUE;
-
         long[] min = new long[nums.length];
-        long[] max = new long[nums.length];
-
         PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
         long currsum=0;
         
@@ -31,13 +28,9 @@ class Solution {
             if(pq.size() > n){
                 currsum-=pq.poll();
             }
-            if(i <= nums.length-n){
-                max[i] = currsum;
+            if(i <= nums.length-n && i >= n){
+                ans = Math.min(ans,min[i-1]-currsum);
             }
-        }
-
-        for(int i=n-1;i<nums.length-n;i++){
-            ans = Math.min(ans,min[i]-max[i+1]);
         }
 
         return ans;
