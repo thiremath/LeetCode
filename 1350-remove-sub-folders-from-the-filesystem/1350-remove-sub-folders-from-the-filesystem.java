4c45b90d58814f8920909ea47f5ab521249e8faf
thiremath <143859257+thiremath@users.noreply.github.com>
2025-07-19 14:34:25 -0400

    Time: 52 ms (46.09%), Space: 55.8 MB (51.76%) - LeetHub

--- 1350-remove-sub-folders-from-the-filesystem/1350-remove-sub-folders-from-the-filesystem.java
+++ 1350-remove-sub-folders-from-the-filesystem/1350-remove-sub-folders-from-the-filesystem.java
@@ -5,30 +5,20 @@ class Solution {
 
         for(String s: folder){
             set.add(s);
-
         }
 
         for(String s: folder){
-            String[] arr = s.split("/");
-            // for(String scurr:arr){
-            //     System.out.println(scurr);
-            // }
-            // System.out.println("len = "+arr.length);
-            // System.out.println("");
-
-            if(arr.length == 2){
+            if(s.split("/").length == 2){
                 ans.add(s);
             }
 
             else{
-                // String temp = "";
                 int index = s.length()-1;
                 while(index > 0){
                     while(s.charAt(index) != '/'){
                         index--;
                     }
                     String curr = s.substring(0,index);
-                    // System.out.println("curr = "+curr);
                     if(set.contains(curr)){
                         break;
                     }
