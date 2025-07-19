1a278dc86357ee0b5e5c3afa03082e69c16b6f52
thiremath <143859257+thiremath@users.noreply.github.com>
2025-07-19 16:43:44 -0400

    Time: 37 ms (91.04%), Space: 53.8 MB (98.61%) - LeetHub

--- 1350-remove-sub-folders-from-the-filesystem/1350-remove-sub-folders-from-the-filesystem.java
+++ 1350-remove-sub-folders-from-the-filesystem/1350-remove-sub-folders-from-the-filesystem.java
@@ -1,34 +1,45 @@
 class Solution {
     public List<String> removeSubfolders(String[] folder) {
-        Set<String> set = new HashSet<>();
+        // Set<String> set = new HashSet<>();
         List<String> ans = new ArrayList<>();
 
-        for(String s: folder){
-            set.add(s);
-        }
+        Arrays.sort(folder);
 
-        for(String s: folder){
-            if(s.split("/").length == 2){
-                ans.add(s);
-            }
+        int high=-1;
+        String prev="-";
 
-            else{
-                int index = s.length()-1;
-                while(index > 0){
-                    while(s.charAt(index) != '/'){
-                        index--;
-                    }
-                    String curr = s.substring(0,index);
-                    if(set.contains(curr)){
-                        break;
-                    }
-                    index--;
-                }
-                if(index <= 0){
-                    ans.add(s);
-                }
+        while(++high < folder.length){
+            if(!folder[high].startsWith(prev)){
+                ans.add(folder[high]);
+                prev = folder[high]+"/";
             }
         }
+
+        // for(String s: folder){
+        //     set.add(s);
+        // }
+
+        // for(String s: folder){
+        //     if(s.split("/").length == 2){
+        //         ans.add(s);
+        //     }
+
+        //     else{
+        //         int index = s.length()-1;
+        //         while(index > 0){
+        //             while(s.charAt(index) != '/'){
+        //                 index--;
+        //             }
+        //             if(set.contains(s.substring(0,index))){
+        //                 break;
+        //             }
+        //             index--;
+        //         }
+        //         if(index <= 0){
+        //             ans.add(s);
+        //         }
+        //     }
+        // }
 
         return ans;
     }
