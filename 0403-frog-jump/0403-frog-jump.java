3bb22a5716f1a748bbae84def4b31d8a0b67691d
thiremath <143859257+thiremath@users.noreply.github.com>
2025-07-23 19:03:40 -0400

    Time: 11 ms (98.26%), Space: 45.9 MB (97.54%) - LeetHub

--- 0403-frog-jump/0403-frog-jump.java
+++ 0403-frog-jump/0403-frog-jump.java
@@ -1,21 +1,16 @@
 class Solution {
     public boolean canCross(int[] stones) {
-        // Integer[][] dp = new Integer[stones.length][2000000];
         Map<Integer,Set<Integer>> dp = new HashMap<>();
         return canCrossWorker(stones,0,0,dp);
-        // return canCrossWorker(stones,0,0);
     }
 
     public boolean canCrossWorker(int[] stones, int index, int prev, Map<Integer,Set<Integer>> dp) {
-
-    // public boolean canCrossWorker(int[] stones, int index, int prev) {
 
         if(index == stones.length-1){
             return true;
         }
 
         if(prev == 0){
-            // return canCrossWorker(stones,1,1,dp);
             if(stones[1] == 1){
                 return canCrossWorker(stones,1,1,dp);
             }
@@ -31,9 +26,14 @@ class Solution {
 
         for(int i=index+1;i<stones.length;i++){
 
-            if(stones[i] >= minJump+stones[index] && stones[i] <= maxJump+stones[index]){
-                if(canCrossWorker(stones,i,Math.abs(stones[i]-stones[index]),dp)){
-                // if(canCrossWorker(stones,i,Math.abs(stones[i]-stones[index]))){
+            int jump = stones[i]-stones[index];
+
+            if(jump > maxJump){
+                break;
+            }
+
+            if(jump >= minJump && jump <= maxJump){
+                if(canCrossWorker(stones,i,stones[i]-stones[index],dp)){
                     return true;
                 }
             }
