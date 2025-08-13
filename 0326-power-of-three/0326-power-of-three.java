88262356cfc754755e091611f2168f097aaba418
thiremath <143859257+thiremath@users.noreply.github.com>
2025-08-12 23:02:55 -0400

    Time: 14 ms (6.99%), Space: 44.4 MB (18.53%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.java
+++ 0326-power-of-three/0326-power-of-three.java
@@ -1,21 +1,5 @@
 class Solution {
     public boolean isPowerOfThree(int n) {
-        String nSt = Integer.toString(n, 3);
-        System.out.println(nSt);
-        boolean flag = false;
-        if(n > 0){
-            for(char c: nSt.toCharArray()){
-                if(c != '1' && c != '0'){
-                    return false;
-                }
-                if(c == '1'){
-                    if(flag){
-                        return false;
-                    }
-                    flag = true;
-                }
-            }
-        }
-        return flag;
+        return (n <= 0) ? false : Integer.toString(n, 3).replace("0","").equals("1");
     }
 }
