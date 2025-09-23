45d4484206c23ead45c55dae9c7912c11f401ee8
thiremath <143859257+thiremath@users.noreply.github.com>
2025-09-23 13:07:29 -0400

    Time: 1 ms (76.03%), Space: 41.7 MB (48.13%) - LeetHub

--- 0165-compare-version-numbers/0165-compare-version-numbers.java
+++ 0165-compare-version-numbers/0165-compare-version-numbers.java
@@ -1,57 +1,33 @@
 class Solution {
     public int compareVersion(String version1, String version2) {
-        int res=0;
-        String[] v1 = version1.split("\\.");
-        String[] v2 = version2.split("\\.");
-        int n1 = v1.length;
-        int n2 = v2.length;
-        // System.out.print(v1.length);
-        // for(String c: v1){
-        //     System.out.print(c+" ");
-        // }
-        // System.out.println("");
-        // for(String c: v2){
-        //     System.out.print(c+" ");
-        // }
-        int pv1=0,pv2=0;
+        String[] v1arr = version1.split("\\.");
+        String[] v2arr = version2.split("\\.");
 
-        while(pv1 < n1 && pv2 < n2){
-            int curr1 = Integer.valueOf(v1[pv1]);
-            int curr2 = Integer.valueOf(v2[pv2]);
+        int v1p=0,v2p=0;
 
-            if(curr1 > curr2){
+        // System.out.println(v1arr.length+" "+v2arr.length);
+
+        while(v1p<v1arr.length || v2p<v2arr.length){
+            int a=0,b=0;
+
+            if(v1p<v1arr.length){
+                a = Integer.valueOf(v1arr[v1p]);
+                v1p++;
+            }
+            if(v2p<v2arr.length){
+                b = Integer.valueOf(v2arr[v2p]);
+                v2p++;
+            }
+
+            // System.out.println(a+" "+b);
+
+            if(a > b){
                 return 1;
             }
-
-            else if(curr1 < curr2){
-                return -1;
-            }
-
-            pv1++;
-            pv2++;
-        }
-
-        if(pv1 < n1){
-            while(pv1 < n1 && Integer.valueOf(v1[pv1])==0){
-                pv1++;
-            }
-            if(pv1 < n1){
-                return 1;
-            }
-        }
-
-        else{
-            while(pv2 < n2 && Integer.valueOf(v2[pv2])==0){
-                pv2++;
-            }
-            if(pv2 < n2){
+            else if(b > a){
                 return -1;
             }
         }
-
         return 0;
     }
 }
-
-// [1],[01]
-// [1],[001]
