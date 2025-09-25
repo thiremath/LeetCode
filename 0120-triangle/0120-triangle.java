55fe292962cf456907cf7a12432fffcbc7608548
thiremath <143859257+thiremath@users.noreply.github.com>
2025-09-25 14:20:04 -0400

    Time: 9 ms (11.07%), Space: 45.7 MB (30.46%) - LeetHub

--- 0120-triangle/0120-triangle.java
+++ 0120-triangle/0120-triangle.java
@@ -1,29 +1,10 @@
 class Solution {
     public int minimumTotal(List<List<Integer>> triangle) {
-
-        List<List<Integer>> dp = new ArrayList<>();
-
-        for(List<Integer> x1 : triangle){
-            List<Integer> temp = new ArrayList<>();
-            for(int i=0;i<x1.size();i++){
-                temp.add(-1);
+        for(int i=triangle.size()-2;i>=0;i--){
+            for(int j=0;j<triangle.get(i).size();j++){
+                triangle.get(i).set(j,triangle.get(i).get(j)+Math.min(triangle.get(i+1).get(j),triangle.get(i+1).get(j+1)));
             }
-            dp.add(temp);
         }
-
-        return minimumTotalWorker(triangle, dp, 0, 0);
-    }
-
-    public int minimumTotalWorker(List<List<Integer>> triangle, List<List<Integer>> dp, int index, int innerindex) {
-
-        if(index == triangle.size() || innerindex == triangle.get(index).size()){
-            return 0;
-        }
-
-        if(dp.get(index).get(innerindex) == -1){
-            dp.get(index).add(innerindex, triangle.get(index).get(innerindex) + Math.min(minimumTotalWorker(triangle, dp, index+1, innerindex), minimumTotalWorker(triangle, dp, index+1, innerindex+1) ));
-        }
-        
-        return dp.get(index).get(innerindex);
+        return triangle.get(0).get(0);
     }
 }
