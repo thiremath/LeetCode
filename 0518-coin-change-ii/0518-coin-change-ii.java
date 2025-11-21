6432c40d7814fafa14e06e539aa4ded5d108e1e2
thiremath <143859257+thiremath@users.noreply.github.com>
2025-11-21 14:07:36 -0500

    Time: 21 ms (20.37%), Space: 58.4 MB (5.03%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.java
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -1,33 +1,55 @@
 class Solution {
     public int change(int amount, int[] coins) {
-        Arrays.sort(coins);
-        int n = coins.length;
-        int[][] dp = new int[n][amount+1];
-        for (int i = 0; i < n; i++) {
-            Arrays.fill(dp[i], -1);
-        }
-        return changeWorker(amount, coins, 0, dp);
-    }
+        // if(coins[index] > amount) dp[amount][index]=0;
 
-    public int changeWorker(int amount, int[] coins, int index, int[][] dp) {
-        if (amount == 0) {
-            return 1;
-        }
+        // dp[amount][index] = dp[amount-coins[index]][index]+ dp[amount][index+1];
 
-        if (amount < 0 || index >= coins.length) {
-            return 0;
+        // dp[amount][0];
+
+        int[][] dp = new int[amount+1][coins.length];
+
+        // when amount =0 0 way
+
+        for(int i=0;i<coins.length;i++){
+            dp[0][i] = 1;
         }
 
-        if (dp[index][amount] != -1) {
-            return dp[index][amount];
+        for(int i=0;i<coins.length;i++){
+            if(coins[i]<=amount){
+                dp[coins[i]][i]=1;
+            }
         }
 
-        if(amount < coins[index]){
-            return dp[index][amount] = 0;
+        int ans=0;
+
+        for(int i=1;i<=amount;i++){
+            for(int j=0;j<coins.length;j++){
+                if(coins[j] > i && j>0) dp[i][j]=dp[i][j-1];
+                else{
+                    // if(i==amount){
+                    //     ans+=dp[i][j];
+                    // }
+                    if(i-coins[j]>0){
+                        dp[i][j] += dp[i-coins[j]][j];
+                    }
+                    if(j>0){
+                        dp[i][j]+= dp[i][j-1];
+                    }
+                }
+            }
         }
 
-        return dp[index][amount] = changeWorker(amount - coins[index], coins, index, dp)
-                + changeWorker(amount, coins, index + 1, dp);
+        // for(int i=0;i<=amount;i++){
+        //     for(int j=0;j<coins.length;j++){
+        //         System.out.print(dp[i][j]+" ");
+        //     }
+        //     System.out.println("");
+        // }
+
+        // System.out.println("ans= "+ans);
+
+        return dp[amount][coins.length-1];
 
     }
 }
+
