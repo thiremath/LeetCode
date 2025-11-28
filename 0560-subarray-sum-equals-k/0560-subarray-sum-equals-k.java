88b336f4c2259e1b7a1ac89f6b8144fd2813702f
thiremath <143859257+thiremath@users.noreply.github.com>
2025-11-27 20:42:58 -0500

    Time: 23 ms (92.2%), Space: 48.6 MB (31.22%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -9,22 +9,45 @@ class Solution {
 
          */
 
-         if(nums.length==0) return 0;
+        // [1,1,2,3,-2,1]
 
-         int res=0,n=nums.length;
+        // 0,1
+        // 1,1
+        // 2,1
+        // 4,1
 
-         for(int i=0;i<n;i++){
+        HashMap<Integer,Integer> sumOccMap = new HashMap<>();
+        int res=0;
 
-            int sum = nums[i];
-            if(sum==k) res++;
+        sumOccMap.put(0,1);
+        int sum=0;
 
-            for(int j=i+1;j<n;j++){
-                sum += nums[j];
-                if(sum==k) res++;
+        for(int num:nums){
+            sum += num;
+            if(sumOccMap.containsKey(sum-k)){
+                res += sumOccMap.get(sum-k);
             }
+            sumOccMap.put(sum,sumOccMap.getOrDefault(sum,0)+1);
+        }
 
-         }
+        return res;
 
-         return res;
+        //  if(nums.length==0) return 0;
+
+        //  int res=0,n=nums.length;
+
+        //  for(int i=0;i<n;i++){
+
+        //     int sum = nums[i];
+        //     if(sum==k) res++;
+
+        //     for(int j=i+1;j<n;j++){
+        //         sum += nums[j];
+        //         if(sum==k) res++;
+        //     }
+
+        //  }
+
+        //  return res;
     }
 }
