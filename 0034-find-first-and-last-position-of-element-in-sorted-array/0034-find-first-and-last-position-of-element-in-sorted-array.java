5126e66745a51d66c11af5ff8f97671a6f092eba
thiremath <143859257+thiremath@users.noreply.github.com>
2025-11-28 19:01:37 -0500

    Time: 7 ms (1.06%), Space: 54.3 MB (8.4%) - LeetHub

---
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -0,0 +1,80 @@
+class Solution {
+    public int[] searchRange(int[] nums, int target) {
+        int low = 0, high = nums.length-1;
+
+        while(low <= high){
+            int mid = low + (high - low) / 2;
+
+            if(target == nums[mid]){
+                int[] res = new int[2];
+                res[0] = findFarthestIndex(nums,mid,true,low,high);
+                res[1] = findFarthestIndex(nums,mid,false,low,high);
+                return res;
+            }
+
+            else if(target < nums[mid]){
+                high = mid - 1;
+            }
+
+            else{
+                low = mid + 1;
+            }
+        }
+
+        return new int[]{-1,-1};
+    }
+
+    public int findFarthestIndex(int[] nums, int index, boolean isLeftMostFarthest, int low, int high){
+        int target = nums[index];
+        int farthestIndex = index;
+
+        // int low=0,high=0;
+
+        // if(isLeftMostFarthest){
+        //     low = 0;
+        //     high = index-1;
+        // }
+
+        // else{
+        //     low = index+1;
+        //     high = nums.length-1;
+        // }
+
+        System.out.println("low,high,bool"+low+" "+high+" "+isLeftMostFarthest);
+
+        while(low <= high){
+            int mid = low + (high-low)/2;
+
+            if(target == nums[mid]){
+                farthestIndex = mid;
+
+                if(isLeftMostFarthest){
+                    high = mid - 1;
+                }
+
+                else{
+                    low = mid + 1;
+                }
+            }
+
+            else if(target < nums[mid]){
+                high = mid - 1;
+            }
+
+            else{
+                low = mid + 1;
+            }
+        }
+
+        return farthestIndex;
+    }
+}
+
+/**
+
+[5,7,8,8,8,8,8,10]
+t=8
+
+
+
+ */
