4f32d7b730f1e23faea8a2c8d9b8655ad4dad123
thiremath <143859257+thiremath@users.noreply.github.com>
2025-12-19 14:51:00 -0500

    Time: 7 ms (11.32%), Space: 48.4 MB (8.1%) - LeetHub

---
+++ 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.java
@@ -0,0 +1,31 @@
+class Solution {
+    List<List<Integer>> FinalRes;
+    List<Integer> currRes;
+
+    public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
+        currRes = new ArrayList<>();
+        FinalRes = new ArrayList<>();
+        dfs(graph,0,new HashSet<>());
+        return FinalRes;
+    }
+
+    public void dfs(int[][] graph, int node, Set<Integer> visitedSet){
+        visitedSet.add(node);
+        currRes.add(node);
+
+        if(node == graph.length-1){
+            FinalRes.add(new ArrayList<>(currRes));
+        }
+
+        else{
+            for(int adjNode:graph[node]){
+                if(!visitedSet.contains(adjNode)){
+                    dfs(graph,adjNode,visitedSet);
+                }
+            }
+        }
+
+        visitedSet.remove(node);
+        currRes.remove(currRes.size()-1);
+    }
+}
