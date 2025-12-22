9b8bc5aa3690a1ac5ce9b7e59968cff9b98f79f0
thiremath <143859257+thiremath@users.noreply.github.com>
2025-12-22 13:23:10 -0500

    Time: 71 ms (10.13%), Space: 55.3 MB (10.31%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -1,79 +1,48 @@
 class Solution {
+
+    private Map<String,Integer> dpMap = new HashMap<>();
+
     public int minDistance(String word1, String word2) {
-        // int[][] dp = new int[word1.length()+1][word2.length()+1];
-        // // boolean[][] isrequired = new boolean[word1.length()+1][word2.length()+1];
 
-        // for(int i=0;i<dp.length;i++){
-        //     dp[i][0] = i;
-        // }
-
-        // for(int i=0;i<dp[0].length;i++){
-        //     dp[0][i] = i;
-        // }
-
-        // dp[word1.length()][word2.length()] = -1;
-
-        // for(int i=dp.length-1;i>=1;i--){
-        //     for(int j=dp[0].length-1;j>=1;j--){
-        //         if(dp[i][j] == -1){
-        //             if(word1.charAt(i-1) != word2.charAt(j-1)){
-        //                 dp[i-1][j] = -1;
-        //                 dp[i][j-1] = -1;
-        //             }
-        //             dp[i-1][j-1] = -1;
-        //         }
-        //     }
-        // }
-
-        // for(int i=1;i<dp.length;i++){
-        //     for(int j=1;j<dp[i].length;j++){
-        //         if(dp[i][j] == -1){
-        //             if(word1.charAt(i-1) == word2.charAt(j-1)){
-        //                 dp[i][j] = dp[i-1][j-1];
-        //             }
-        //             else{
-        //                 dp[i][j] = 1 + Math.min( Math.min( dp[i-1][j],dp[i][j-1] ),dp[i-1][j-1] );
-        //             }
-        //         }
-        //     }
-        // }
-
-
-        // return dp[word1.length()][word2.length()];
-
-        int[][] dp = new int[word1.length()+1][word2.length()+1] ;
-
-        for(int[] i: dp){
-            Arrays.fill(i,-1);
+        // insertion
+        if(word1.length()==0 || word2.length()==0){
+            if(word1.length()==0) return word2.length();
+            return word1.length();
         }
 
-        return minDistWorker(word1,word2,word1.length(),word2.length(),dp);
+        String st = String.valueOf(new StringBuilder(word1).append(" ").append(word2));
 
+        if(dpMap.containsKey(st)) return dpMap.get(st);        
 
-    }
+        int res = Integer.MAX_VALUE;
 
-    public int minDistWorker(String word1, String word2, int i, int j, int[][] dp) {
-
-        if(dp[i][j] == -1){
-            if(i == 0){
-                dp[i][j] = j;
-            }
-
-            else if(j == 0){
-                dp[i][j] = i;
-            }
-
-            else if(word1.charAt(i-1) == word2.charAt(j-1)){
-                dp[i][j] = minDistWorker(word1,word2,i-1,j-1,dp);
-            }
-
-            else{
-                dp[i][j] = 1 + Math.min( minDistWorker(word1,word2,i-1,j-1,dp), Math.min( minDistWorker(word1,word2,i-1,j,dp), minDistWorker(word1,word2,i,j-1,dp) ) );
-            }
+        // if first char same call rec
+        if(word1.charAt(0) == word2.charAt(0)){
+            res = minDistance(word1.substring(1,word1.length()),word2.substring(1,word2.length()));
         }
 
-        return dp[i][j] ;
+        // diff first chars
+        else{
+            // delete first char of word1
+            res = Math.min(res,1+minDistance(word1.substring(1,word1.length()),word2));
 
+            // delete first char of word2
+            res = Math.min(res,1+minDistance(word1,word2.substring(1,word2.length())));
+
+            // perform replace
+
+            res = Math.min(res,1+minDistance(word1.substring(1,word1.length()),word2.substring(1,word2.length())));
+        }
+
+        dpMap.put(st,res);
+
+        return res;
     }
-
 }
+
+
+/**
+
+abc , kbc
+
+ */
